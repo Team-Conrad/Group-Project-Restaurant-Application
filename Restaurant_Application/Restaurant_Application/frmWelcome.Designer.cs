202c7d2645ceead2234aef34b81d04569182a27c
Dan-Williams93 <dannywiliams@hotmail.co.uk>
2015-03-26 18:22:50 +0000

    testable front of house syestem (BETA)

--- Restaurant_Application/Restaurant_Application/frmWelcome.Designer.cs
+++ Restaurant_Application/Restaurant_Application/frmWelcome.Designer.cs
@@ -37,23 +37,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(275, 42);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.Size = new System.Drawing.Size(226, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Zizzi\'s";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(234, 84);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 99);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.Size = new System.Drawing.Size(397, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Please Enter the number of diners";
             // 
             // cmnOptions
             // 
+            this.cmnOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmnOptions.FormattingEnabled = true;
             this.cmnOptions.Items.AddRange(new object[] {
             "1",
@@ -66,18 +69,19 @@
             "8",
             "9",
             "10"});
-            this.cmnOptions.Location = new System.Drawing.Point(368, 151);
+            this.cmnOptions.Location = new System.Drawing.Point(237, 176);
             this.cmnOptions.Name = "cmnOptions";
-            this.cmnOptions.Size = new System.Drawing.Size(193, 24);
+            this.cmnOptions.Size = new System.Drawing.Size(193, 37);
             this.cmnOptions.TabIndex = 17;
             this.cmnOptions.SelectedIndexChanged += new System.EventHandler(this.cmnOptions_SelectedIndexChanged);
             // 
             // btnAccept
             // 
             this.btnAccept.Enabled = false;
-            this.btnAccept.Location = new System.Drawing.Point(433, 367);
+            this.btnAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccept.Location = new System.Drawing.Point(248, 343);
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.Size = new System.Drawing.Size(182, 79);
             this.btnAccept.TabIndex = 18;
             this.btnAccept.Text = "Enter";
             this.btnAccept.UseVisualStyleBackColor = true;
@@ -92,6 +96,7 @@
             this.Controls.Add(this.cmnOptions);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmWelcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
